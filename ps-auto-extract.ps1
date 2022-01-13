5e1b4c9f6daf106298ab8e9230bef18915c681c3
Chris McCauley <cdmccauley@gmail.com>
2022-01-13 16:27:59 -0700

    removed multiple watchers as they stopped working, replaced with a list and if statement. Also, I noticed that the 7z module I was previously using was not retaining file structure and would break IDE projects so I researched and found that 7z.exe can be used if it is in path with an x flag to retain file structure.

--- ps-auto-extract.ps1
+++ ps-auto-extract.ps1
@@ -1,42 +1,20 @@
 # https://stackoverflow.com/questions/29066742/watch-file-for-changes-and-run-command-with-powershell
-# https://gist.github.com/mobzystems/793007db28e3ffcc20e2#file-7-zip-psm1
+
+# TODO: check if 7z in path
 
 $folder = "C:\Users\Chris\Downloads"
-$_7zFilter = "*.7z"
-$_zipFilter = "*.zip"
-$_rarFilter = "*.rar"
 
 Set-Location $folder
 
-$_7zWatcher = New-Object IO.FileSystemWatcher $folder, $_7zFilter -Property @{ 
+$watcher = New-Object IO.FileSystemWatcher $folder -Property @{ 
     IncludeSubdirectories = $false
     NotifyFilter = [IO.NotifyFilters]'FileName, LastWrite'
 }
 
-$_7zOnCreated = Register-ObjectEvent $_7zWatcher -EventName Created -SourceIdentifier _7zOnCreated -Action {
+$onCreated = Register-ObjectEvent $watcher -EventName Created -SourceIdentifier FileCreated -Action {
+    $extensions = @(".7z", ".zip", ".rar")
     $path = $Event.SourceEventArgs.FullPath
-    $target = [System.IO.Path]::GetFileNameWithoutExtension($path)
-    Expand-7zArchive $path $target
-}
-
-$_zipWatcher = New-Object IO.FileSystemWatcher $folder, $_zipFilter -Property @{ 
-    IncludeSubdirectories = $false
-    NotifyFilter = [IO.NotifyFilters]'FileName, LastWrite'
-}
-
-$_zipOnCreated = Register-ObjectEvent $_zipWatcher -EventName Created -SourceIdentifier _zipOnCreated -Action {
-    $path = $Event.SourceEventArgs.FullPath
-    $target = [System.IO.Path]::GetFileNameWithoutExtension($path)
-    Expand-7zArchive $path $target
-}
-
-$_rarWatcher = New-Object IO.FileSystemWatcher $folder, $_rarFilter -Property @{ 
-    IncludeSubdirectories = $false
-    NotifyFilter = [IO.NotifyFilters]'FileName, LastWrite'
-}
-
-$_rarOnCreated = Register-ObjectEvent $_rarWatcher -EventName Created -SourceIdentifier _rarOnCreated -Action {
-    $path = $Event.SourceEventArgs.FullPath
-    $target = [System.IO.Path]::GetFileNameWithoutExtension($path)
-    Expand-7zArchive $path $target
+    $extension = [System.IO.Path]::GetExtension($path)
+    $isArchive = $extensions.Contains($extension)
+    if ($isArchive) { 7z x $path }
 }

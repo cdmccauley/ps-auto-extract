6b579822e7d8ea5aba894aae29aa2940795a00f2
Chris McCauley <cdmccauley@gmail.com>
2022-01-13 14:12:26 -0700

    instead of trying to create a filter to cover all extensions switched to a watcher for each extension

--- ps-auto-extract.ps1
+++ ps-auto-extract.ps1
@@ -2,14 +2,40 @@
 # https://gist.github.com/mobzystems/793007db28e3ffcc20e2#file-7-zip-psm1
 
 $folder = "C:\Users\Chris\Downloads"
-$filter = "*.*z*"
+$_7zFilter = "*.7z"
+$_zipFilter = "*.zip"
+$_rarFilter = "*.rar"
 
-$Watcher = New-Object IO.FileSystemWatcher $folder, $filter -Property @{ 
+Set-Location $folder
+
+$_7zWatcher = New-Object IO.FileSystemWatcher $folder, $_7zFilter -Property @{ 
     IncludeSubdirectories = $false
     NotifyFilter = [IO.NotifyFilters]'FileName, LastWrite'
 }
 
-$onCreated = Register-ObjectEvent $Watcher -EventName Created -SourceIdentifier FileCreated -Action {
+$_7zOnCreated = Register-ObjectEvent $_7zWatcher -EventName Created -SourceIdentifier _7zOnCreated -Action {
+    $path = $Event.SourceEventArgs.FullPath
+    $target = [System.IO.Path]::GetFileNameWithoutExtension($path)
+    Expand-7zArchive $path $target
+}
+
+$_zipWatcher = New-Object IO.FileSystemWatcher $folder, $_zipFilter -Property @{ 
+    IncludeSubdirectories = $false
+    NotifyFilter = [IO.NotifyFilters]'FileName, LastWrite'
+}
+
+$_zipOnCreated = Register-ObjectEvent $_zipWatcher -EventName Created -SourceIdentifier _zipOnCreated -Action {
+    $path = $Event.SourceEventArgs.FullPath
+    $target = [System.IO.Path]::GetFileNameWithoutExtension($path)
+    Expand-7zArchive $path $target
+}
+
+$_rarWatcher = New-Object IO.FileSystemWatcher $folder, $_rarFilter -Property @{ 
+    IncludeSubdirectories = $false
+    NotifyFilter = [IO.NotifyFilters]'FileName, LastWrite'
+}
+
+$_rarOnCreated = Register-ObjectEvent $_rarWatcher -EventName Created -SourceIdentifier _rarOnCreated -Action {
     $path = $Event.SourceEventArgs.FullPath
     $target = [System.IO.Path]::GetFileNameWithoutExtension($path)
     Expand-7zArchive $path $target
